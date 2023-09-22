f90fd456c767103bffc6f6ba2c5f2fb7c1db6275
Piotr <pioncz@gmail.com>
2023-09-22 18:03:53 +0200

    fixed fonts issue

--- src/game/assets/AssetsLoaderHelpers.ts
+++ src/game/assets/AssetsLoaderHelpers.ts
@@ -8,3 +8,8 @@ export enum FontNames {
   ExpressionPro = 'ExpressionPro',
   m5x7 = 'm5x7',
 }
+
+export const FontPaths: Record<FontNames, string> = {
+  ExpressionPro: 'fonts/ExpressionPro.woff',
+  m5x7: 'fonts/m5x7.ttf',
+};
