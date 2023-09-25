ebbbfb922c5329cf7593e9dd032028cfb64c51f4
Piotr <pioncz@gmail.com>
2023-09-25 15:48:14 +0200

    db data flow

--- src/game/GameComponent.tsx
+++ src/game/GameComponent.tsx
@@ -2,20 +2,34 @@ import { useEffect, memo, useContext } from 'react';
 import Game from './Game';
 import { styled } from '@stitches/react';
 import GameDbContext from 'game-ui/contexts/GameDbContext';
+import throttle from 'lodash/throttle';
+import { PlayerStatistics, Point } from './utils/Types';
 
 const GameComponent = () => {
-  const { name, setGameInstance, setStatistics } =
-    useContext(GameDbContext);
+  const {
+    name,
+    position,
+    setGameInstance,
+    setStatistics,
+    setPosition,
+  } = useContext(GameDbContext);
 
   useEffect(() => {
-    const g = new Game({ containerId: 'game', name });
-
-    g.on('playerUpdate', (playerStatistics) => {
+    const g = new Game({ containerId: 'game', name, position });
+    const statsHandler = (playerStatistics: PlayerStatistics) => {
       setStatistics(playerStatistics);
-    });
+    };
+    const positionHandler = throttle((newPosition: Point) => {
+      setPosition(newPosition);
+    }, 1000);
+
+    g.on('playerUpdate', statsHandler);
+    g.on('positionUpdate', positionHandler);
     setGameInstance(g);
 
     return () => {
+      g.off('playerUpdate', statsHandler);
+      g.off('positionUpdate', positionHandler);
       g.remove();
       setGameInstance();
     };
