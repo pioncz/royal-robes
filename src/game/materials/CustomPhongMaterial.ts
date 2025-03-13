a082d593ce960fc244006927dc7dbbaeece3a554
Piotr <pioncz@gmail.com>
2025-03-13 14:29:39 +0100

    w

---
+++ src/game/materials/CustomPhongMaterial.ts
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+
+const createCustomPhongMaterial = (
+  params: THREE.MeshPhongMaterialParameters,
+) => {
+  const newMaterial = new THREE.MeshPhongMaterial({
+    ...params,
+    onBeforeCompile: (shader) => {
+      console.log(shader.fragmentShader);
+      shader.fragmentShader = shader.fragmentShader.replace(
+        '#include <lights_phong_pars_fragment>',
+        `varying vec3 vViewPosition;
+
+struct BlinnPhongMaterial {
+
+	vec3 diffuseColor;
+	vec3 specularColor;
+	float specularShininess;
+	float specularStrength;
+
+};
+
+void RE_Direct_BlinnPhong( const in IncidentLight directLight, const in vec3 geometryPosition, const in vec3 geometryNormal, const in vec3 geometryViewDir, const in vec3 geometryClearcoatNormal, const in BlinnPhongMaterial material, inout ReflectedLight reflectedLight ) {
+
+	float dotNL = abs( dot( geometryNormal, directLight.direction ) );
+	vec3 irradiance = dotNL * directLight.color;
+
+	reflectedLight.directDiffuse += irradiance * BRDF_Lambert( material.diffuseColor );
+
+	reflectedLight.directSpecular += irradiance * BRDF_BlinnPhong( directLight.direction, geometryViewDir, geometryNormal, material.specularColor, material.specularShininess ) * material.specularStrength;
+
+}
+
+void RE_IndirectDiffuse_BlinnPhong( const in vec3 irradiance, const in vec3 geometryPosition, const in vec3 geometryNormal, const in vec3 geometryViewDir, const in vec3 geometryClearcoatNormal, const in BlinnPhongMaterial material, inout ReflectedLight reflectedLight ) {
+
+	reflectedLight.indirectDiffuse += irradiance * BRDF_Lambert( material.diffuseColor );
+
+}
+
+#define RE_Direct				RE_Direct_BlinnPhong
+#define RE_IndirectDiffuse		RE_IndirectDiffuse_BlinnPhong`,
+      );
+    },
+  });
+  return newMaterial;
+};
+
+export default createCustomPhongMaterial;
